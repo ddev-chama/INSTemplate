8e5b6f122d0644d842473c1821969fe18ffd7e8d
virus2540 <ak3167@gmail.com>
2019-01-17 16:24:25 +0700

    Last Th_Day 4.30pm

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,12 +1,36 @@
 <div style="padding: 50px">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d496116.4743852624!2d100.35196580677203!3d13.723901084527709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29c55e8121673%3A0x8714604f44efb220!2sInternational+Network+System+Co.%2C+Ltd.!5e0!3m2!1sth!2sth!4v1547626608345" width="600" height="200" frameborder="0" align="left" style="border:0;margin-right: 30px;" allowfullscreen=""></iframe>
-    <div class="elementor-text-editor elementor-clearfix">
-        <p><span style="color: #000000;"><strong>สำนักงานใหญ่</strong></span><br><span style="color: #000000;">เลขที่ 95/28
-                ถนนบางกรวย-ไทรน้อย</span><br><span style="color: #000000;">ตำบลบางเลน อำเภอบางใหญ่ จังหวัดนนทบุรี</span></p>
-        <p><span style="color: #000000;"><strong>ศูนย์บริการและสำนักงานขาย</strong></span><br><span style="color: #000000;">เลขที่
-                1420/1 อาคารศรีสุข ชั้น 4 ถนนพหลโยธิน</span><br><span style="color: #000000;">แขวงจอมพล เขตจตุจักร
-                กรุงเทพมหานคร 10900</span></p>
-    </div>
+    <div style="padding-left:200px">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d496116.4743852624!2d100.35196580677203!3d13.723901084527709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29c55e8121673%3A0x8714604f44efb220!2sInternational+Network+System+Co.%2C+Ltd.!5e0!3m2!1sth!2sth!4v1547626608345" width="400" height="300" frameborder="0" align="left" style="border:0;margin-right: 30px;" allowfullscreen=""></iframe>
+        <div class="elementor-text-editor elementor-clearfix" style="padding-top:40px">
+            <p><span style="color: #000000;"><strong>สำนักงานใหญ่</strong></span><br><span style="color: #000000;">เลขที่ 95/28
+                    ถนนบางกรวย-ไทรน้อย</span><br><span style="color: #000000;">ตำบลบางเลน อำเภอบางใหญ่ จังหวัดนนทบุรี</span></p>
+            <p><span style="color: #000000;"><strong>ศูนย์บริการและสำนักงานขาย</strong></span><br><span style="color: #000000;">เลขที่
+                    1420/1 อาคารศรีสุข ชั้น 4 ถนนพหลโยธิน</span><br><span style="color: #000000;">แขวงจอมพล เขตจตุจักร
+                    กรุงเทพมหานคร 10900</span></p>
+        </div></div>
     <div data-id="iqr515h" class="elementor-element elementor-element-iqr515h elementor-widget elementor-widget-icon-list" data-element_type="icon-list.default"><div class="elementor-widget-container"><ul class="elementor-icon-list-items"><li class="elementor-icon-list-item"> <span class="elementor-icon-list-icon"> <i class="fa fa-phone" aria-hidden="true"></i> </span> <span class="elementor-icon-list-text">02-513-7637</span></li><li class="elementor-icon-list-item"> <span class="elementor-icon-list-icon"> <i class="fa fa-fax" aria-hidden="true"></i> </span> <span class="elementor-icon-list-text">02-513-7686</span></li><li class="elementor-icon-list-item"> <span class="elementor-icon-list-icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </span> <span class="elementor-icon-list-text">info@itns.co.th, hr@itns.co.th</span></li></ul></div>
     </div>
-</div>
+        <div id="disqus_thread" style="padding-left:200px;padding-right:200px;"></div>
+    </div>
+
+
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://chama-k.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                            
